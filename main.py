d4fe3db2e56b2ef215a34b39ff585d12f724e518
MaxAFriedrich <58374387+MaxAFriedrich@users.noreply.github.com>
2023-04-06 16:41:10 +0100

    Corretly transibes large blocks of text in not quite real time.

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import threading
 
-import keyboard
 import numpy as np
 import sounddevice as sd
 import soundfile as sf
+from pynput import keyboard
 
 import whisper
 from whisper.audio import SAMPLE_RATE
@@ -35,9 +35,9 @@ def record(duration: float, sr: int = SAMPLE_RATE):
     return audio.flatten()
 
 
-def transcribe(frame_number: int, audio, old_audio):
+def transcribe(frame_number: int, audio):
     # concatinate audio
-    audio = whisper.pad_or_trim(np.concatenate([old_audio, audio]))
+    audio = whisper.pad_or_trim(audio)
 
     # make log-Mel spectrogram and move to the same device as the model
     mel = whisper.log_mel_spectrogram(audio).to(MODEL.device)
@@ -76,57 +76,51 @@ def check_for_repeating_chars(text):
             return False  # Words are not all the same
     return True  # All words are the same
 
-def splice_at_deviation(old_text, text):
-    """
-    This function takes in two strings `old_text` and `text`, finds the first character that differs in `text`,
-    and splices `old_text` and `text` together at that first deviation.
 
-    Args:
-    old_text: string, the original text that we want to splice into.
-    text: string, the new text that we want to splice into `old_text`.
+def bulk_transcribe(start: int, end: int):
+    global audio_buffer
+    full_audio = np.array([], dtype=np.float32)
+    for i in range(start, end + 1):
+        full_audio = np.concatenate([full_audio, audio_buffer.get(i)])
+    return transcribe(end, full_audio)
 
-    Returns:
-    A string that represents the spliced `old_text` and `text` together at their first deviation.
-    """
-    for i in range(min(len(old_text), len(text))):
-        if old_text[i] != text[i]:
-            return old_text[:i] + text[i:]
 
-    # If all characters in `old_text` and `text` are the same, return the whole `text`
-    return text
-
+# np.concatenate([old_audio, audio])
 def transcribe_main(frame_size: float):
     global STOP
-    old_text = ""
-    merged_text = ""
-    old_audio = np.array([], dtype=np.float32)
-    stop_counter = 0
     frame_counter = 0
+    current_start = None
+    current_end = None
     print("Transcribing")
     while not STOP:
         audio = audio_buffer.get(frame_counter)
         if audio is None:
             sleep(frame_size / 3)
         else:
-            text = transcribe(frame_counter, audio, old_audio)
+            text = transcribe(frame_counter, audio)
             if not (
                 text == ""
                 or text == None
                 or all(c in ". " for c in text)
                 or check_for_repeating_chars(text)
             ):
-                old_text = text
-                merged_text = splice_at_deviation(merged_text,text)
-                output(frame_counter, merged_text)
-                old_audio = audio
+                if current_start == None:
+                    output(frame_counter, text)
+                    current_end = current_start = frame_counter
+                else:
+                    current_end = frame_counter
+                    new_text = bulk_transcribe(current_start, current_end)
+                    if not (new_text == None or new_text == ""):
+                        text = new_text
+                    output(frame_counter, text)
                 stop_counter = 0
             else:
-                merged_text = ""
-                stop_counter += 1
-                print(stop_counter)
-                if stop_counter > 3:
-                    STOP = True
-            audio_buffer.pop(frame_counter)
+                if current_start == None or current_end == None:
+                    audio_buffer.pop(frame_counter)
+                else:
+                    for i in range(current_start, current_end + 1):
+                        audio_buffer.pop(i)
+                current_start = current_end = None
             frame_counter += 1
     print("Not transcribing")
 
@@ -135,20 +129,8 @@ def output(frame_no: int, text: str):
     print(f"{frame_no}: {text}")
 
 
-def shortcut_listener(shortcut: str):
+def main(block_size: float = 3):
     global STOP
-
-    def stop():
-        global STOP
-        STOP = True
-
-    keyboard.add_hotkey(shortcut, stop)
-    keyboard.wait()
-
-
-def main(block_size: float = 5, shortcut: str = "ctrl+z"):
-    global STOP
-    # threading.Thread(target=shortcut_listener, args=[shortcut]).start()
     threading.Thread(target=transcribe_main, args=[block_size]).start()
     print("Listening...")
     try:
