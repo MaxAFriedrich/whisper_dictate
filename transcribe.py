f70953ab799627b1f9735955dff41e8e688f6e20
MaxAFriedrich <58374387+MaxAFriedrich@users.noreply.github.com>
2023-04-13 12:40:04 +0100

    moved transcription into sperate file

---
+++ transcribe.py
@@ -0,0 +1,30 @@
+import whisper
+class Transcribe:
+    def __init__(self,model) -> None:
+        self.model = model
+    def run(self,frame_number: int, audio):
+        # concatinate audio
+        audio = whisper.pad_or_trim(audio)
+    
+        # make log-Mel spectrogram and move to the same device as the model
+        mel = whisper.log_mel_spectrogram(audio).to(self.model.device)
+    
+        # decode the audio
+        # joined_memory = "".join(frame_memory)
+        options = whisper.DecodingOptions(
+            fp16=False,
+            language="en",
+            prompt=" ",
+            temperature=0,
+        )
+        # run the transcription
+        try:
+            result = whisper.decode(self.model, mel, options)
+            text = result.text
+        except RuntimeError:
+            print(f"Failed to transcribe {frame_number}.")
+            return None
+        if type(text) != str:
+            return None
+        return text
+    
